9785e92d4aa43d25efcee2a045b918e4ef1330f2
KaviGamer <69410834+KaviGamer@users.noreply.github.com>
2021-02-11 17:42:26 +0530

    Add files via upload

---
+++ screens/ReadStoryScreen.js
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import {
+  View,
+  TextInput,
+  Text,
+} from 'react-native';
+import { Header } from 'react-native-elements';
+
+export default class Write extends React.Component {
+    render(){
+        return(
+    <View>
+<Text>Read Story</Text>
+    </View>
+        )
+    }
+}
